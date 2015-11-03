39459d0ab0ca17a0a5ba00e14c1d381b323a0da5
Jihann <1244098192@qq.com>
2015-11-03 20:55:49 +0800

    添加管理员权限

---
+++ controllers/auth_controller.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Jihann on 2015/11/2.
+ * 用户权限中间件
+ */
+
+//用户登录权限
+exports.signinRequired = function(req, res, next) {
+    var loginUser = req.session.user;
+    if (!loginUser) {
+        res.redirect('/signin');
+    }
+    next();
+};
+
+//管理员权限
+exports.adminRequired = function(req, res, next) {
+    var loginUser = req.session.user;
+    if (loginUser && loginUser.role <= 10) {
+        return res.redirect('/signin');
+    }
+    next();
+};
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var Auth = require('../controllers/auth_controller');
 var User = require('../controllers/user_controller');
+var Post = require('../controllers/post_controller');
 
 // Index
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', Post.list);
+
+// Post
+router.get('/post', Post.info);
+router.post('/post', Post.create);
 
 // User
+router.post('/user/signin', User.signin);
 router.post('/user/signup', User.signup);
 router.get('/signin', User.showSignin);
+router.get('/signup', User.showSignup);
+router.get('/logout', User.logout);
+router.get('/admin/user/list', Auth.signinRequired, Auth.adminRequired, User.list);
+
+// 账户设置
+router.get('/user/mod', User.userModInfo);
+router.post('/user/mod', User.userMod);
+
+// 头像上传
+router.get('/mod/head/:id', User.headInfo);
+router.post('/mod/head', User.saveHead, User.headMod);
 
 module.exports = router;
